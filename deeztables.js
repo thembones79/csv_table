555efb5f93c07381cdef92f9a0efec42f0f63637
thembones79 <michal.szumnarski@gmail.con>
2024-07-22 17:32:36 +0200

    added dummy data

--- deeztables.js
+++ deeztables.js
@@ -1,63 +1,15 @@
-const renderTable = (data) => {
-    const cols = getColumns(data)
-    const header = renderHeader(cols)
-    const body = renderTableBody({ data, cols })
-    return `<div class="table-container"><table>${header}${body}</table></div><div class="footer"><button class="btn" onclick="onSave(this)">Save</button></div>`
-}
+const myWorker = new Worker('worker.js')
 
-const getColumns = (data) => Object.keys(data[0])
-
-const renderHeader = (cols) => {
-    const columns = cols.map((c) => `<th>${c}</th>`).join('')
-    return `<thead><tr>${columns}</tr></thead>`
-}
-
-const renderTableBody = ({ data, cols }) =>
-    `<tbody>${data.map((row) => renderRow({ row, cols })).join('')}</tbody>`
-
-const renderRow = ({ row, cols }) => {
-    const columns = cols
-        .map(
-            (c) =>
-                `<td><input placeholder="${row[c]}" title="${row[c]}" value="${row[c]}" onchange="checkMeDaddy(this)" /></td>`
-        )
-        .join('')
-    return `<tr>${columns}</tr>`
-}
-
-function checkMeDaddy({ placeholder, value, classList }) {
-    if (placeholder === value) {
-        classList.remove('diff-values')
-    } else {
-        classList.add('diff-values')
-        document.querySelector('button.btn').innerText = 'Save'
-    }
-}
-
-async function getData() {
-    const url = '/ligma-deez.json'
-    try {
-        const response = await fetch(url)
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`)
-        }
-
-        const data = await response.json()
-        return data
-    } catch (error) {
-        console.error(error.message)
-    }
-}
-
-const onSave = async (btn) => {
-    console.log(btn)
-    btn.innerText = 'Data was saved'
-}
+const root = document.getElementById('root')
 
 const onLoad = async () => {
-    const data = await getData()
-    const table = renderTable(data)
-    document.getElementById('root').outerHTML = table
+    myWorker.postMessage('root')
+    console.log('Message posted to worker')
+}
+
+myWorker.onmessage = (e) => {
+    console.log('Message received from worker', e)
+    document.getElementById('root').innerHTML = e.data
 }
 
 window.onload = onLoad
